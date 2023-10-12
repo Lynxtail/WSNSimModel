ab2bd86c43b7c318c3a8bfb36be15b5af1cf1ae0
Lynxtail <l.gorbachev@mail.ru>
2023-10-12 20:39:30 +0400

    rewrite system class

--- Systems.py
+++ Systems.py
@@ -3,18 +3,16 @@ from math import log, prod
 from numpy import random
 
 class QueueingSystem:
-    serviced_demands = dict()
-    # {id_demand:
-    #       (arrival_time, begin_service, end_service, from_id, where_id)}
-
-    def __init__(self, id:int, server_cnt:int, mu:float, gamma:float, queue_length:int=10, state:bool=True, k:int=1) -> None:
+    def __init__(self, id:int, server_cnt:int, mu:float, gamma:float, state:bool=True, k:int=1) -> None:
         self.id = id
         self.server_cnt = server_cnt
         self.mu = mu
-        self.queue_length = queue_length
         self.gamma = gamma
-        self.state = state
         self.k = k
+        self.state = state # работоспособна
+        self.service_flag = False # свободна
+        self.demands = list()
+        self.time_states = list()
     
     def service_time(self):
         return -log(prod([random.random() for _ in range(self.k)])) / self.mu
@@ -24,14 +22,8 @@ class QueueingSystem:
     
     def serialization(self):
         with open(f'system_{id}.pickle', 'wb') as f:
-            pickle.dump(self.serviced_demands, f)
+            pickle.dump(self.time_states, f)
     
     def deserialization(self):
         with open(f'system_{id}.pickle', 'rb') as f:
-            self.serviced_demands = pickle.load(f)
-
-
-class OuterQueueingSystem(QueueingSystem):
-    def __init__(self, id: int, lambda_0:float, server_cnt: int, mu: float, gamma: float, queue_length: int = 10, state: bool = True, k: int = 1) -> None:
-        super().__init__(id, server_cnt, mu, gamma, queue_length, state, k)
-        self.lambda_0 = lambda_0
+            return pickle.load(f)
