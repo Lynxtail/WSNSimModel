a6797c4a5ac2b6cbcd15aba4aba6e31b2580add4
Lynxtail <l.gorbachev@mail.ru>
2023-10-12 20:38:43 +0400

    change node notes

--- Demand.py
+++ Demand.py
@@ -1,10 +1,10 @@
 # описание требования
 class Demand:
-    def __init__(self, id:int, nodes:list, arrival:float=0, exit:float=0):
+    def __init__(self, id:int, arrival:float=0, exit:float=0):
         self.id = id
         # момент поступления в сеть
         self.arrival = arrival
         # момент выхода из сети 
         self.exit = exit
         # список посещенных узлов сети
-        self.nodes = nodes
+        self.nodes = list()
