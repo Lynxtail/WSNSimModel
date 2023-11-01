e193f574c492d22d1eac95c4213b599b8c82e3d8
Lynxtail <l.gorbachev@mail.ru>
2023-11-01 20:53:47 +0400

    change parameters

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from math import tau
 from QueueingNetwork import QueueingNetwork
 import numpy as np
 
@@ -14,7 +15,11 @@ if __name__ == '__main__':
         [0, 1, 0, 0, 0, 0, 0, 0, 0]
     ])
     mu = (5, 1.8, 1., 1.7, 1., 1.9, 1.2, 3.1)
+    # gamma = (.001, .001, .001, .001, .001, .001, .001, .001)
     gamma = (.01, .02, .04, .03, .01, .02, .04, .03)
+    # gamma = (1.1, 1.2, 1.4, 1.3, 1.1, 1.2, 1.4, 1.3)
+    tau_threshold = 10
+    lambda_0 = 1.5
 
     # theta = np.array([
     #     [0, .5, .5],
@@ -23,6 +28,5 @@ if __name__ == '__main__':
     # ])
     # mu = (1.5, 1.3)
     # gamma = (.01, .02)
-    lambda_0 = 1.5
-    nw = QueueingNetwork(10**1, len(mu), lambda_0, theta, mu, gamma)
+    nw = QueueingNetwork(10**2, len(mu), lambda_0, theta, mu, gamma, tau_threshold)
     nw.simulation()
